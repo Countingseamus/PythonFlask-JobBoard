c96888da896e5d60f0faf3bcfed25ece1213afc2
Seamus Halton <seamus2304@outlook.com>
2019-12-30 15:39:26 +0000

    Added macros, db, display jobs

--- jobs/app.py
+++ jobs/app.py
@@ -32,4 +32,9 @@ def close_connection(exception):
 @app.route('/')
 @app.route('/jobs')
 def jobs():
-    return render_template('index.html')
+    command = """
+    SELECT job.id, job.title, job.description, job.salary, employer.id as employer_id,
+    employer.name as employer_name FROM job JOIN employer ON employer.id = job.employer_id;
+    """
+    jobs = execute_sql(command)
+    return render_template('index.html', jobs=jobs)
