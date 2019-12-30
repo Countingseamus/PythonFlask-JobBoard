fd0d38693f846ee4573ba5d111d4ae1dac8380fa
Seamus Halton <seamus2304@outlook.com>
2019-12-30 15:43:48 +0000

    Removing redundant semi-colon in DB SELECT

--- jobs/app.py
+++ jobs/app.py
@@ -34,7 +34,7 @@ def close_connection(exception):
 def jobs():
     command = """
     SELECT job.id, job.title, job.description, job.salary, employer.id as employer_id,
-    employer.name as employer_name FROM job JOIN employer ON employer.id = job.employer_id;
+    employer.name as employer_name FROM job JOIN employer ON employer.id = job.employer_id
     """
     jobs = execute_sql(command)
     return render_template('index.html', jobs=jobs)
